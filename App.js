a926e5d7f9198802647039119f41f9df6d1772f8
MAC <user@MACs-MacBook-Pro.local>
2024-12-30 09:01:32 +0100

    This is a goals or todo app

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, Text, View, FlatList, ScrollView } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+    setCourseGoals((currGoalText) => [...currGoalText, enteredGoalText]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your goals' onChangeText={goalInputHandler}/>
+        <Button title='Add Goals' onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalsContainer}>
+        <ScrollView>
+        {courseGoals.map((goal) => <Text style={styles.goalItem} key={goal}>{goal}</Text>)}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -13,8 +33,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    padding:5,
+    margin:5,
+    borderRadius:8,
+    borderColor: '#cccccc',
+    backgroundColor: '#5e0acc',
+    color: 'white',
+  }
 });
